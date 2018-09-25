cefb6f0f2a3034ee5ccc7ebc998d6ebb45382fc3
eltonpeniche <elton_nfs@hotmail.com>
2018-09-25 15:53:01 -0300

    Função Turn

--- src/combat/Combat.java
+++ src/combat/Combat.java
@@ -1,10 +1,48 @@
 package combat;
 
 import characters.Entity;
-public class Combat {
+public abstract class Combat {
 	
 	
-	public boolean hit(Entity attacker, Entity defender) {
+	public static void turn(Entity player, Entity monster) {
+		int turn_player = 0;
+		int turn_monster = 0;
+		int damage = 0;
+		while((player.attributes.hp > 0)||(monster.attributes.hp > 0)) {
+			System.out.println(player.getName() + ": " + player.attributes.hp);
+			System.out.println(monster.getName() + ": " + monster.attributes.hp);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			turn_player += player.attributes.atkSpeed;
+			if (turn_player >= 10) {
+				turn_player -= 10;
+				if (hit(player, monster)) {
+					damage = (int)Math.random()*100 +0;
+					System.out.println(player.getName() + " deu " + damage + " de dano em " + monster.getName() + "!");
+					monster.updateHp(-damage);
+				}else {
+					System.out.println(player.getName() + "errou!");
+				}
+			}
+			turn_monster += player.attributes.atkSpeed;
+			if (turn_monster >= 10) {
+				turn_monster -= 10;
+				if (hit(player, monster)) {
+					damage = (int)Math.random()*100 +0;
+					System.out.println(monster.getName() + " deu " + damage + " de dano em " + player.getName() + "!");
+					player.updateHp(-damage);
+				}else {
+					System.out.println(player.getName() + "errou!");
+				}
+			}
+		}
+	}
+	
+	public static boolean hit(Entity attacker, Entity defender) {
 		int chance = (int)Math.random()*100 +0;
 		//ATTACKERHIT - DEFENDERFLEE
 		int precision = attacker.attributes.hit - defender.attributes.flee;
