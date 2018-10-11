38458394e99702d087ff7b2c2d4c2795d429035c
Alberto Sobrinho <aclsobrinho@hotmail.com>
2018-10-11 15:14:06 -0300

    New Classes

--- src/characters/Attributes.java
+++ src/characters/Attributes.java
@@ -1,56 +1,31 @@
 package characters;
-
 import combat.Combat;
 
 //A Class designed to hold standard attributes for all characters from the game
-public abstract class Entity {
+public class Attributes {
 
-	public class Attributes{
-		public int strength = 2;
-		public int vitality = 2;
-		public int dexterity = 2;
-		public int intelligence = 2;
-		public int luck = 2;
-		public int agility = 2;
-		public int power = 2;
-		
-		public int hp = 50;
-		public int sp = 2;
-		public int atk = 10;
-		public int def = 5;
-		public int atkM = 2;
-		public int defM = 2;
-		public int hit = 10;
-		public int crit=  2;
-		public int tenacity = 2;
-		public int flee = 1;
-		public int atkSpeed = 3;
-		
-		public Attributes() {
-			
-		}
-	}
+	public int strength = 2;
+	public int vitality = 2;
+	public int dexterity = 2;
+	public int intelligence = 2;
+	public int luck = 2;
+	public int agility = 2;
+	public int power = 2;
 	
-	public Attributes attributes = new Attributes();
-	public String status = "Free";
-	private String name;
-	public String race;
-	public int level = 1;
-	
-	
-	public int attack(Entity target) {
+	public int hp = 50;
+	public int sp = 2;
+	public int atk = 10;
+	public int def = 5;
+	public int atkM = 2;
+	public int defM = 2;
+	public int hit = 10;
+	public int crit=  2;
+	public int tenacity = 2;
+	public int flee = 1;
+	public int atkSpeed = 3;
 		
-		if (this.attributes.hp > 0) {
-			int damage = attributes.atk - target.attributes.def;
-			if(damage > 0){
-				target.updateHp(-damage);
-				return damage;
-			}
-			else {
-				return 0;
-			}
-		}
-		return 0;
+	public Attributes() {
+		
 	}
 	
 	public void upLevel(int level){
@@ -67,19 +42,4 @@ public abstract class Entity {
 			this.attributes.hp = 0;
 		}
 	}
-	
-	//This function returns the character's stats
-	public Attributes getAttributes() {
-		return this.attributes;
-	}
-	
-	//This function returns the name of the character
-	public String getName() {
-		return this.name;
-	}
-	//Constructor class
-	public Entity(String name) {
-		this.name = name;
-	}
-	
 }
--- src/characters/Character.java
+++ src/characters/Character.java
@@ -2,61 +2,10 @@ package characters;
 
 import java.util.*;
 import items.*;
+import characters.Attributes;
 
-
-public class Character extends Entity{
+public abstract class Character {
 	
-
-	public class Bag{
-		int capacity = 40;
-		int used = 0;
-		ArrayList<Item> items = new ArrayList <Item>();
-		
-		
-		public void addItem(Item item){
-			if(used < capacity) {
-				items.add(item);
-				used++;
-			}
-		}
-		
-		public void delItem(Item item) {
-			for(Item i : items) {
-				if (i.id == item.id) {
-					items.remove(i);
-				}
-			}
-			
-		}
-		
-		public void list() {
-			for(Item i : items) {
-				System.out.println("Name: " + i.name);
-				System.out.println("Description: " + i.description);
-			}
-		}
-		public Bag() {
-			
-		}
-	}
-	
-	
-	public Bag bag;
-	
-	public Character(String name, String gender) {
-		super(name);
-		this.gender = gender;
-		this.bag= new Bag();
-		// TODO Auto-generated constructor stub
-	}
-
-	String gender;
-	int exp;
-	
-	
-	
-	
-	
-	
+	Attributes attributes = new Attributes();
 
 }
