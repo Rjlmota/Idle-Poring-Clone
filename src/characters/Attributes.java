ca459a2db2eb0a2f4f0e2dad783182984c321ef9
Alberto Sobrinho <aclsobrinho@hotmail.com>
2018-10-11 16:32:24 -0300

    Attributes

--- src/characters/Attributes.java
+++ src/characters/Attributes.java
@@ -3,40 +3,56 @@ package characters;
 
 //A Class designed to hold standard attributes for all characters from the game
 public class Attributes {
+	public int power;
+	
+	public int strength;
+	public int agility;
+	public int vitality;
+	public int intelligence;
+	public int dexterity;
+	public int luck;
 
+	public int maxHp;
+	public int maxSp;
 	
-	public int level = 1;
-	
-	public int strength = 2;
-	public int vitality = 2;
-	public int dexterity = 2;
-	public int intelligence = 2;
-	public int luck = 2;
-	public int agility = 2;
-	public int power = 2;
-	
-	public int hp = 50;
-	public int sp = 2;
-	public int atk = 10;
-	public int def = 5;
-	public int atkM = 2;
-	public int defM = 2;
-	public int hit = 10;
-	public int crit=  2;
-	public int tenacity = 2;
-	public int flee = 1;
-	public int atkSpeed = 3;
+	public int atk;
+	public int def;
+	public int atkM;
+	public int defM;
+	public int hit;
+	public int evasion;
+	public int critic;
+	public int atkSpeed;
+	public int tenacity;
+	public int cast;
 		
-	public Attributes() {
-		
+	public Attributes(String id) {
+		if (id.charAt(0) == '1') {
+			this.strength = 9;
+			this.agility = 9;
+			this.vitality = 9;
+			this.intelligence = 9;
+			this.dexterity = 9;
+			this.luck = 9;
+		}
+		upAttributes(id);
 	}
-	
-	public void upLevel(int level){
-		this.level += level;
-		this.atk += this.level*2;
-		this.hit += (int)this.level*1.5;
-		this.hp += this.level*3;
-		this.flee += this.level*2;
+	public void upAttributes(String id) {
+		if (id.charAt(1) == '1') {
+			this.maxHp = (2*this.strength) + (12*this.agility) + (16*this.vitality) + 
+					(8*this.intelligence) + (8*this.dexterity) + (10*this.luck);
+			this.maxSp = (2*this.strength) + (8*this.intelligence) + (8*this.dexterity);
+			this.atk = (4*this.strength);
+			this.def = (2*this.vitality) + (2*this.intelligence);
+			this.atkM = 0;
+			this.defM = (2*this.vitality) + (2*this.intelligence) + (2*this.dexterity);
+			this.hit = (2*this.luck);
+			this.evasion = (2*this.agility);
+			this.critic = (2*this.luck);
+			this.atkSpeed = (2*this.agility);
+			this.tenacity = (2*this.luck);
+			this.cast = 0;
+		}
 	}
 	
 	public void updateHp(int damage){
