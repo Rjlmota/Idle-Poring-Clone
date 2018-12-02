4889e9fdc2f35967f07747f2798f4dd5973ee10e
Rjlmota <rjlmota@gmail.com>
2018-12-02 11:13:26 -0300

    update

--- src/characters/Class.java
+++ src/characters/Class.java
@@ -11,17 +11,13 @@ import java.util.Map;
 
 public class Class extends Entity{
 	
-	//To DO: extend equipments.
-	public Item[] equipments = new Item[8];
+
 	
 	
 	public Bag bag = new Bag();
 	
 	public String id; // 1-Swordsman; 2-Wizard; 3-Archer
-	
-	public int power;
-	
-	private String[] index = {"str", "agi", "vit", "int", "dex", "luk", "power"};
+
 	
 	public Class(String name, String id) {
 		super(name);
@@ -36,34 +32,6 @@ public class Class extends Entity{
 		updateClass();
 	}
 
-	public void updateClass() {
-		Map<String,Integer> attr = this.attr.getAttributes();
-		Map<String,Integer> equips = getEquipAttributes();
-		Map<String,Integer> stats = new HashMap<String,Integer>();
-		
-		for (String key : attr.keySet()) {
-		    stats.put(key, attr.get(key) + equips.get(key));			
-		}
-		
-		this.stats.setStats(stats);
-	}
-	
-	public Map<String,Integer> getEquipAttributes() {
-		Map<String,Integer> equip ;
-		Map<String,Integer> total = new HashMap<String,Integer>();
-		
-		for (int i=0; i<7; i++) {
-		    total.put(index[i], 0);
-		}
-
-		for (int i=0; i<8; i++) {
-			equip = this.equipments[i].attr.getAttributes();
-			for (String key : total.keySet()) {
-				total.put(key, total.get(key) + equip.get(key));		
-			}
-		}
-		return total;
-	}
 	
 	public void collectLoot(ArrayList <Item> loot) {
 		for(Item item : loot) {
